4345b2fcca86210da8f4c6db769b751ed5150e60
RAVI KUMAR ISHWAR <159942657+RAVIKUMARISHWAR03@users.noreply.github.com>
2025-07-24 23:55:51 +0530

    PRACTICE

---
+++ TOPICS/Array.java
@@ -0,0 +1,17 @@
+public class Array {
+    public static void main(String[] args) {
+       // int[] MyArr = new int[5];
+       /* MyArr[0] = 45;
+        MyArr[4] = 7 ;
+        MyArr[3] = 18;
+        MyArr[2] = 1;
+        MyArr[1] = 17;*/
+        int[] MyArr ={45,7,18,1,17};
+        System.out.println(MyArr[0]);
+        System.out.println(MyArr[1]);
+        System.out.println(MyArr[3]);
+        System.out.println(MyArr[4]);
+        System.out.println(MyArr[2]);
+
+    }
+}
